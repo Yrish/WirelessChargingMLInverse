b649759de21b0d0815fcb01cd69b66e266a77bda
Byron Johnson <byron@byronjohnson.net>
2020-04-10 16:59:06 -0400

    example.py: Print mean and standard deviation of errors.

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ from torch.utils.data import Dataset
 from torch import from_numpy
 import numpy as np
 
+# CSV: 7 nn inputs then 5 nn outputs
+# 5 nn outputs: V_PriWind, kdiff, Bleak, Pout, Vcore
 class Data(Dataset):
     def __init__(self, csv_path: str = '../data/data.csv', startingIndex: int = 1, inputCount: int = 7, switchInputAndOutput: bool = False) -> "Data":
         self.inputCount: int = inputCount
@@ -10,6 +12,7 @@ class Data(Dataset):
         self.switchInputAndOutput: bool = switchInputAndOutput
         self.data = pd.read_csv(csv_path)
         self.outputCount = self.data.count(axis=1).iloc[0] - startingIndex - inputCount
+        #print("Debug: self.outputCount is", self.outputCount)
 
     def __len__(self):
         return len(self.data)
